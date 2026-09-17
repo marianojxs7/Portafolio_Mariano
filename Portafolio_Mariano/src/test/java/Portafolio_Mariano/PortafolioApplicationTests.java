b6026f7a2730a4a4aa46d73b761c9e7fdaaad9c7
marianojxs7 <marianojimenez7@hotmail.com>
2026-09-16 20:38:47 -0600

    Semana 2

---
+++ Portafolio_Mariano/src/test/java/Portafolio_Mariano/PortafolioApplicationTests.java
@@ -0,0 +1,13 @@
+package Portafolio_Mariano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortafolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
